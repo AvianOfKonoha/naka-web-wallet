58541ba68e520d8010bb5019131b0ad072cc1cae
AvianOfKonoha <marko.fink@gmail.com>
2025-06-30 11:45:45 +0200

    Update local serve file

--- start_WIN.bat
+++ start_WIN.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d %~dp0
-start "" http://localhost:8000
+start "" http://localhost:8000/naka-dapp-vue
 python\python.exe serve_dist.py
 pause
--- start_macOS.command
+++ start_macOS.command
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-open http://localhost:8000
+open http://localhost:8000/naka-dapp-vue
 python3 serve_dist.py
