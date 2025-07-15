7c966a6d1943a7680f07b37795db972e2e34d570
BreakfastBurrito <marko.fink@elly.com>
2025-07-15 13:45:53 +0200

    Remove unnecessary route

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,14 +8,6 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
-    },
-    {
-      path: '/contracts',
-      name: 'contracts',
-      // route level code-splitting
-      // this generates a separate chunk (ContractsView.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/ContractsView.vue')
     }
   ]
 });
