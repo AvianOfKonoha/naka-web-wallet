cad59dcba66ee26a6ac9ea6328fae1299906aea8
AvianOfKonoha <marko.fink@gmail.com>
2026-07-21 09:34:24 +0200

    increase decimal points on amount

--- src/components/Withdraw/Form/DefineAmount.vue
+++ src/components/Withdraw/Form/DefineAmount.vue
@@ -73,7 +73,7 @@ watch(
           :required="props.amountData.required"
           min="0"
           @input="preventNegative"
-          step="0.00001"
+          step="0.000001"
           v-model="props.amountData.value"
         />
         <button
